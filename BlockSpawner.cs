26ee524efae0de390f2629c648373dc4c2e32f20
louismeunier <louismeunier123@gmail.com>
2021-02-25 20:54:28 -0500

    init

---
+++ BlockSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    public GameObject[] Tetrominoes;
+    
+    void Start()
+    {
+        NewTetromino();
+    }
+
+    public void NewTetromino()
+    {
+        Instantiate(Tetrominoes[Random.Range(0, Tetrominoes.Length)], transform.position, Quaternion.identity);
+    }
+}
+
